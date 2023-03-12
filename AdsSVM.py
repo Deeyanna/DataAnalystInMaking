63e9674849f8ebeaad22b6adf9cc8e47a5c8bd5e
Deeyanna <127641649+Deeyanna@users.noreply.github.com>
2023-03-12 14:56:48 +0800

    Add files via upload

---
+++ AdsSVM.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.write("""
+# Sales Prediction based on Advertisment Budget of Each Medium App
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 0.7, 296.4, 10.0)
+    Radio = st.sidebar.slider('Radio', 0.0, 49.6, 5.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 6.9, 0.6)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+loaded_model = pickle.load(open("AdvertisingSVM.h5", "rb"))
+
+prediction = loaded_model.predict(df)
+
+st.subheader('Prediction')
+st.write(prediction)
